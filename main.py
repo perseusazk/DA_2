84ef83a597bcbcdb28c4b71783a0dbc99c7f1113
Azri <azkaerial@gmail.com>
2022-02-24 23:21:58 +0800

    Patch of missing unittest and missing scrapy code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# import requests library
+# import library
+import unittest
+
 import requests
+import scrapy
+
 #import scrapy to run scrapy code below
-
-# Target url
 url = "https://brickset.com/sets/year-2011"
+# Target url
 r = requests.get(url)
-
 
 # Getting the headers of the target website
 h = requests.head(url)
@@ -20,18 +22,43 @@ for x in h.headers:
     print("\t ", x, ":", h.headers[x])
 print("****")
 
+#to enable unittest
+class TestCase(unittest.TestCase):
+    def testExample(self):
+        print("")
 
 
-import scrapy
+
+
+
+
+
+
+
+
+
+
+
+
 
 class NewSpider(scrapy.Spider):
    name = "new_spider"
-   start_urls = ["https://brickset.com/sets/year-2011"]
-
+   start_urls = ['http://brickset.com/sets/year-2011']
    def parse(self, response):
-      css_selector = 'img'
-      for x in response.css(css_selector):
+      xpath_selector = '//img'
+      for x in response.xpath(xpath_selector):
          newsel = '@src'
          yield {
-                 'Image Link': x.xpath(newsel).extract_first(),
+            'Image Link': x.xpath(newsel).extract_first(),
          }
+# To recurse next page
+      Page_selector = '.next a ::attr(href)'
+      next_page = response.css(Page_selector).extract_first()
+      if next_page:
+         yield scrapy.Request(
+            response.urljoin(next_page),
+            callback=self.parse
+      )
+
+if __name__ == "__main__":
+    unittest.main()
